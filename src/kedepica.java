0beceac7ac907f437038e2b397a93e689dae50f3
rejnsKED <asknelogical@gmail.com>
2021-02-23 22:04:37 +0200

    Pievienotas izvēles

--- src/kedepica.java
+++ src/kedepica.java
@@ -3,7 +3,15 @@ public class kedepica {
 	
 
 		public static int lielums(int pica) {
-			
+			int lielums=0;
+			lielums=Integer.parseInt(JOptionPane.showInputDialog("Picas lielumi = 30cm, 60cm"));
+			if(lielums == 60 || lielums==30) {
+			JOptionPane.showMessageDialog(null,"Picas lielums ir "+lielums);
+			}
+			else{
+			JOptionPane.showMessageDialog(null,"Tāds lielums nepastāv "+lielums);
+			}
+			pica=lielums;
 			return pica;
 		}
 
@@ -12,7 +20,7 @@ public class kedepica {
 			int pica;
 			double pievesana;
 			do {
-				izvele = JOptionPane.showInputDialog("pica/pilna cena/pieve�ana/piedevas");
+				izvele = JOptionPane.showInputDialog("pica/pilna cena/pievešana/piedevas");
 				switch(izvele) {
 				case "pica":
 				pica=lielums(pica);
@@ -20,7 +28,7 @@ public class kedepica {
 				case "pilna cena":
 				pilnacena=cena(pica);
 				break;
-				case "pieve�ana":
+				case "pievešana":
 				pievesana=pievesanasC(pica);
 				break;
 				case "piedevas":
@@ -29,10 +37,7 @@ public class kedepica {
 				}
 				
 			}while(!izvele.equals("beigt"));
-			
-			
-			
-			
+
 			
 		}
 

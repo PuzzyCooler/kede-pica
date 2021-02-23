4182048f7c795f78b8009295fae39596a13a3bcd
rejnsKED <asknelogical@gmail.com>
2021-02-23 22:05:34 +0200

    Pievienota lieluma metode

--- src/kedepica.java
+++ src/kedepica.java
@@ -4,12 +4,12 @@ public class kedepica {
 
 		public static int lielums(int pica) {
 			int lielums=0;
-			lielums=Integer.parseInt(JOptionPane.showInputDialog("Picas lielumi = 30cm, 60cm"));
+			lielums=Integer.parseInt(JOptionPane.showInputDialog("Picas lielumi = 30cm, 60cm."));
 			if(lielums == 60 || lielums==30) {
-			JOptionPane.showMessageDialog(null,"Picas lielums ir "+lielums);
+			JOptionPane.showMessageDialog(null,"Picas lielums ir. "+lielums);
 			}
 			else{
-			JOptionPane.showMessageDialog(null,"Tāds lielums nepastāv "+lielums);
+			JOptionPane.showMessageDialog(null,"Tāds lielums nepastāv. "+lielums);
 			}
 			pica=lielums;
 			return pica;
@@ -20,7 +20,7 @@ public class kedepica {
 			int pica;
 			double pievesana;
 			do {
-				izvele = JOptionPane.showInputDialog("pica/pilna cena/pievešana/piedevas");
+				izvele = JOptionPane.showInputDialog("pica/pilna cena/pievešana/piedevas.");
 				switch(izvele) {
 				case "pica":
 				pica=lielums(pica);
